73be92d64f86c8ba522d851bc1c0c2ca0184522d
searchme86 <searchme86@gmail.com>
2022-12-04 17:29:05 +0900

    🚀 project09 done

---
+++ projects/project09/app.js
@@ -0,0 +1,15 @@
+const URL = 'https://randomuser.me/api/';
+
+import { getElement } from './utils/getElement.js';
+import { getUser } from './utils/fetchUser.js';
+import { displayUser } from './utils/displayUser.js';
+
+const btn = getElement('.btn');
+
+const showUser = async () => {
+  const person = await getUser();
+  displayUser(person);
+};
+
+window.addEventListener('DOMContentLoaded', showUser);
+btn.addEventListener('click', showUser);
---
+++ projects/project09/utils/fetchUser.js
@@ -0,0 +1,28 @@
+const URL = 'https://randomuser.me/api/';
+
+export const getUser = async () => {
+  const response = await fetch(URL);
+  const data = await response.json();
+
+  const person = data.results[0];
+  const { phone, email } = person;
+  const { large: image } = person.picture;
+  const { password } = person.login;
+  const { first, last } = person.name;
+  const {
+    dob: { age },
+  } = person;
+  const {
+    street: { number, name },
+  } = person.location;
+
+  return {
+    phone,
+    email,
+    image,
+    password,
+    age,
+    street: `${number} ${name}`,
+    name: `${first} $${last}`,
+  };
+};
